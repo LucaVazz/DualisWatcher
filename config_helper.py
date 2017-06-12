1cf6969131be8cfd2a2c1390509b6779264689e6
Luca Vazzano <luca.vazzano@gmx.net>
2017-06-13 00:22:50 +0200

    added configuration with CLI and external save

---
+++ config_helper.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+
+class ConfigHelper:
+    def __init__(self):
+        self._whole_config = {}
+
+    def is_present(self):
+        return os.path.isfile('config.json')
+
+    def load(self):
+        try:
+            with open('config.json', 'r') as f:
+                config_raw = f.read()
+                self._whole_config = json.loads(config_raw)
+        except IOError:
+            raise ValueError('No config found!')
+
+    def _save(self):
+        with open('config.json', 'w+') as f:
+            config_formatted = json.dumps(self._whole_config, indent=4)
+            f.write(config_formatted)
+
+    def get_mail_config(self) -> {str : str}:
+        try:
+            return self._whole_config['mail']
+        except KeyError:
+            raise ValueError('Mail-Config is not yet configured!')
+
+    def set_mail_config(self, config: {str: str}):
+        self._whole_config.update({'mail': config})
+        self._save()
+
+    def get_token(self) -> str:
+        try:
+            return self._whole_config['token']
+        except KeyError:
+            raise ValueError('A token is not yet configured!')
+
+    def set_token(self, token: str):
+        self._whole_config.update({'token': token})
+        self._save()
